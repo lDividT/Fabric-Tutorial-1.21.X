df7d765bb162d409b7b097d596f89406716434f4
lDividT <tanioka132@gmail.com>
2025-06-04 23:22:44 -0700

    Added the first item, pvtoken, into our mod

---
+++ src/main/java/net/david/tutorialmod/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.david.tutorialmod.item;
+
+import net.david.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PV_TOKEN = registerItem("pv_token", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+    public static void registerModItems(){
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+           entries.add(PV_TOKEN);
+        });
+    }
+}
